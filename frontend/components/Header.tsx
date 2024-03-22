9693c56478cbeeb83f5af13fc6e1abab2dddfbf5
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-03-22 18:37:27 +0800

    #10 Add Header component
    #10

---
+++ frontend/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  console.log('Header')
+  return (
+    <>
+      <header className="flex items-center justify-center w-full h-22 border mb-10 p-8">
+        header...
+      </header>
+    </>
+  )
+}
