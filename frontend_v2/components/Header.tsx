ae18cecb23aaa68e30e54235313116cc98b80db0
dalaoqi <mystes3016@gmail.com>
2024-03-23 00:05:37 +0800

    refactor: move to fronted_v2

--- frontend_v2/components/Header.tsx
+++ frontend_v2/components/Header.tsx
@@ -1,11 +1,11 @@
-
-
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
 export default function Header() {
   console.log('Header')
   return (
     <>
-      <header className="flex items-center justify-center w-full h-22 border-b mb-10 p-4">
-        Header...
+      <header className="flex items-center justify-end w-full h-22 border mb-10 p-8">
+        <ConnectButton />
       </header>
     </>
   )
