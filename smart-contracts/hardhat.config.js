1697dcbe635f1f97d7ca6cf7204cb8254103854e
robert <juroberttyb@gmail.com>
2024-03-23 18:28:43 +0800

    update: polygon cardona deployed contract address

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -8,10 +8,15 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    optimism_sepolia: {
+    optimism_sepolia: { // deployed with hardhat, checked with remix
       url: "https://opt-sepolia.g.alchemy.com/v2/zEzHSmTu1FlFSvd4P_Yzb21XaZBilczk", // Update with your Infura project ID
       accounts: [PRIVATE_KEY],
       chainId: 11155420,
     },
+    polygon_cardona: { // deployed with remix, checked with remix
+        url: "https://polygonzkevm-cardona.g.alchemy.com/v2/iGQ3hCKslg45YnwfZhP37RQXiixTz_2g", // Update with your Infura project ID
+        accounts: [PRIVATE_KEY],
+        chainId: 2442,
+      },
   },
 };
