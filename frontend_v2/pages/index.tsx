d20a2ca372122c5e684511c9e9c3d38db4e0b847
robert <juroberttyb@gmail.com>
2024-03-24 13:57:45 +0800

    add: cross chain support

--- frontend_v2/pages/index.tsx
+++ frontend_v2/pages/index.tsx
@@ -11,6 +11,22 @@ export default function Home() {
   const orgAddress = optimismDeployment.dao_contract
   console.log('orgAddress:', orgAddress)
 
+  const scrollDeployment = require('../../smart-contracts/deployments/scroll-sepolia.json')
+  const scrollAddress = scrollDeployment.dao_contract
+  console.log('scrollAddress:', scrollAddress)
+
+  const lineaDeployment = require('../../smart-contracts/deployments/linea-goerli.json')
+  const lineaDAddress = lineaDeployment.dao_contract
+  console.log('lineaDAddress:', lineaDAddress)
+
+  const polygonDeployment = require('../../smart-contracts/deployments/polygon-cardona.json')
+  const polygonAddress = polygonDeployment.dao_contract
+  console.log('polygonAddress:', polygonAddress)
+
+  const thunderDeployment = require('../../smart-contracts/deployments/thundercore_testnet.json')
+  const thunderAddress = thunderDeployment.dao_contract
+  console.log('thunderAddress:', thunderAddress)
+
   return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-green-200">
         <Header />
@@ -23,7 +39,35 @@ export default function Home() {
           <Link href={`/orgs/${orgAddress}`} className="mt-8 rounded-2xl shadow-2xl">
             <Image alt='Frog DAO' src={'/transparent.png'} width={400} height={400} className='rounded-t-2xl' />
             <div className='bg-white rounded-b-2xl p-4 text-4xl'>
-              Hacker DAO
+              Hacker DAO - OP
+            </div>
+          </Link>
+
+          <Link href={`/orgs/${scrollAddress}`} className="mt-8 rounded-2xl shadow-2xl">
+            <Image alt='Frog DAO - Scroll' src={'/transparent.png'} width={400} height={400} className='rounded-t-2xl' />
+            <div className='bg-white rounded-b-2xl p-4 text-4xl'>
+              Hacker DAO - Scroll
+            </div>
+          </Link>
+
+          <Link href={`/orgs/${polygonAddress}`} className="mt-8 rounded-2xl shadow-2xl">
+            <Image alt='Frog DAO - Polygon' src={'/transparent.png'} width={400} height={400} className='rounded-t-2xl' />
+            <div className='bg-white rounded-b-2xl p-4 text-4xl'>
+              Hacker DAO - Polygon
+            </div>
+          </Link>
+
+          <Link href={`/orgs/${lineaDAddress}`} className="mt-8 rounded-2xl shadow-2xl">
+            <Image alt='Frog DAO - Linea' src={'/transparent.png'} width={400} height={400} className='rounded-t-2xl' />
+            <div className='bg-white rounded-b-2xl p-4 text-4xl'>
+              Hacker DAO - Linea
+            </div>
+          </Link>
+
+          <Link href={`/orgs/${thunderAddress}`} className="mt-8 rounded-2xl shadow-2xl">
+            <Image alt='Frog DAO - ThunderCore' src={'/transparent.png'} width={400} height={400} className='rounded-t-2xl' />
+            <div className='bg-white rounded-b-2xl p-4 text-4xl'>
+              Hacker DAO - ThunderCore
             </div>
           </Link>
 
